ff6d21bfe72260af54317fad8db815151d3b81eb
C1262322 Gilnei Aquino da Paixao <c1262322@interno.bb.com.br>
2021-10-20 20:46:33 -0300

    inclusao da rota para o formulario de alunos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AlunosComponent } from './alunos/alunos.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'cadastrar-aluno',component: AlunosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AlunosComponent } from './alunos/alunos.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlunosComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
